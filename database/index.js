5db194a9a7cfa2e4b5022b1c1cc3f49fca7e5e88
Akshaj Mody <akshajmody@gmail.com>
2020-04-24 16:21:45 -0700

    Schema made - Helper partially completed

--- database/index.js
+++ database/index.js
@@ -3,14 +3,20 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  //owner.login
+  id: Number, //repo ID
+  username: String, //username
+  //repo full name example : akshajmody/toy_problems
+  full_name: String, //repo full name
+  forks_count: Number //number of forks
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (err, repo) => {
+
 }
+
+//function for get top 25 based on forks
 
 module.exports.save = save;
